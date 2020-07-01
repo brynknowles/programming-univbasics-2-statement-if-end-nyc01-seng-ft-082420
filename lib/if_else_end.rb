d95fc0de3fe9bebe81064026e140552b77d5fdca
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-01 19:22:57 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -9,18 +9,18 @@
 # end
 
 # cleanup the code by trying this solution:
-if Time.now.to_i % 2 == 0
-  puts "Even!"
-else
-  puts "Odd!"
-end
-
-# solution Two:
-# current_time = Time.now
-# current_time = current_time.to_i
- 
-# if current_time.even?
+# if Time.now.to_i % 2 == 0
 #   puts "Even!"
 # else
 #   puts "Odd!"
 # end
+
+# solution Two:
+current_time = Time.now
+current_time = current_time.to_i
+ 
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
